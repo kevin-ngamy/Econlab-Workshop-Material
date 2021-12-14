2140ea5c4fb337a5bdb638da8a28ce20337cd04b
Kevin CGrow <cgrowresearch@gmail.com>
2021-12-14 12:48:00 +0800

    asdfasd

--- HouseDataMaterial.R
+++ HouseDataMaterial.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 library(tidyverse)
 library(ggplot2)
 
@@ -23,11 +24,11 @@ any(is.na(HousingData))
 any(is.na(HousingData$CrimeRate))  #or by variable
 
 #Let's create new variable to express another thing, i.e: variable to indicate if HouseAge > 40
-HousingData$OldHouse <- ifelse(HousingData$HouseAge >= 40, 1, 0)
+HousingData$OldHouse <- ifelse(HousingData$HouseAge >= 35, "Old", "Young")
 
 #Let's try filtering dataset based on the variable (one condition)
-HouseOldData <- filter(HousingData, HouseAge >= 40) #method #1
-HouseOldData <- HouseOldData[HouseOldData$HouseAge >= 40,] #method #2
+HouseOldData <- filter(HousingData, HouseAge >= 35) #method #1
+HouseOldData <- HouseOldData[HouseOldData$HouseAge >= 35,] #method #2
 
 #Filtering dataset on multiple conditions
 #Suppose we want to filter datapoints for houses who are non-old and has high Industrial acres
@@ -49,20 +50,61 @@ plot(HousingData$medv)
 #Cute vizualisasion using ggplot
 
 #Suppose we want vizualise Avg room with respect to House age
-RoomToAgePlot <- ggplot(HousingData, aes(x = DistToCityCenter, y = CrimeRate, color = as.factor(OldHouse))) + geom_point() + scale_y_log10()
+RoomToAgePlot <- ggplot(HousingData, aes(x = DistToCityCenter, y = CrimeRate, color = as.factor(OldHouse))) + geom_point() + 
+  scale_y_log10() + scale_x_log10() + 
+  ggtitle("Distance to City to Crime Rate plot") + xlab("Distance to City Center") + ylab("Crime rate") + facet_wrap(~chas)
 RoomToAgePlot
 
+RoomToAgePlot <- ggplot(HousingData, aes(x = DistToCityCenter, y = CrimeRate)) + geom_point(aes(color = AvgRoom)) + 
+  scale_y_log10() + scale_x_log10() + stat_smooth(method = "lm") +
+  ggtitle("Distance to City to Crime Rate plot") + xlab("Distance to City Center") + ylab("Crime rate") + facet_wrap(~ as.factor(OldHouse))
+RoomToAgePlot
 
+#histograms
+AgePlot <- ggplot(HousingData, aes(x = AvgRoom, fill = as.factor(OldHouse))) + geom_histogram(binwidth = 0.5, position = "dodge") + 
+  ggtitle("Average number of Rooms")
+AgePlot
 
+FacetPlot <- ggplot(HousingData, aes(x = AvgRoom, fill = as.factor(OldHouse))) + geom_histogram(binwidth = 0.5, position = "dodge") + 
+  ggtitle("Average number of Rooms") + facet_wrap(~as.factor(OldHouse))
+FacetPlot
 
+### Statistical Analysis ###
 
+#Running linear model
+RegressionMod <- lm(medv ~ ., data = HousingData)
+summary(RegressionMod)
 
+RegressionMod <- lm(medv ~ CrimeRate + IndustryProp + chas + nox + AvgRoom + DistToCityCenter + rad + 
+                      tax + ptratio + black + lstat, data = HousingData)
+summary(RegressionMod)
 
+#Model diagnostics
+hist(residuals(RegressionMod))
+plot(residuals(RegressionMod))
 
+#Aggregate diagnostic plot
+plot(RegressionMod)
 
+#Analyze multicollinearity
+car::vif(RegressionMod)
 
+RegressionMod <- lm(medv ~ CrimeRate + IndustryProp + chas + nox + AvgRoom + DistToCityCenter + rad:tax + 
+                      ptratio + black + lstat, data = HousingData)
+summary(RegressionMod)
 
+#Run model prediction
+ModelPrediction <- predict(object = RegressionMod, newdata = HousingData, type = "response")
 
+#Attach predictions output from the model to the main data
+HousingData$PredictedMedv <- ModelPrediction
+
+#Compare the results between prediction and observed medv value
+MedvPredObsPlot <- ggplot(HousingData, aes(x = medv, y = PredictedMedv)) + geom_point() + stat_smooth(method = "lm") + ggtitle("Predicted Vs Observed")
+MedvPredObsPlot
+
+#See correlation between Y_hat
+cor(HousingData$medv, HousingData$PredictedMedv)
 
 
 
