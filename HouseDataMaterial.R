87fd6f7f0de9a42a4e045de21115d03f41b8d8e9
Kevin CGrow <cgrowresearch@gmail.com>
2021-12-15 22:26:24 +0800

    asdfasdfadf

--- HouseDataMaterial.R
+++ HouseDataMaterial.R
@@ -1,10 +1,15 @@
 rm(list = ls())
 library(tidyverse)
 library(ggplot2)
+library(corrplot)
+library(ggcorrplot)
+library(caTools)
 
 #Read in csv dataset
 HousingData <- read.csv("~/PersonalProj-Kevin/Econlab/BostonHousingData.csv")
 str(HousingData)
+#see first rows
+head(HousingData)
 
 ### Data Wrangling ###
 HousingData$X <- NULL #Remove useless column
@@ -16,11 +21,13 @@ colnames(HousingData)[3] <- "IndustryProp"
 colnames(HousingData)[6] <- "AvgRoom" 
 colnames(HousingData)[7] <- "HouseAge" 
 colnames(HousingData)[8] <- "DistToCityCenter" 
+colnames(HousingData)[14] <- "HousePrice"
 
 #Check any missing values in the data
 any(is.na(HousingData))
 
 any(is.na(HousingData$CrimeRate))  #or by variable
+any(is.na(HousingData$HousePrice))  #or by variable
 
 #Let's create new variable to express another thing, i.e: variable to indicate if HouseAge > 40
 HousingData$OldHouse <- ifelse(HousingData$HouseAge >= 35, "Old", "Young")
@@ -32,9 +39,16 @@ HouseOldData <- HouseOldData[HouseOldData$HouseAge >= 35,] #method #2
 #Filtering dataset on multiple conditions
 #Suppose we want to filter datapoints for houses who are non-old and has high Industrial acres
 summary(HousingData) #Aggregate summary
+summary(HousingData$HousePrice) #variable summary
 summary(HousingData$IndustryProp) #variable summary
 
-HousingDataHighIndusAndRoom <- HousingData[HousingData$IndustryProp > 11 & HousingData$AvgRoom > 6,]
+#let's create another new variable in the data using Dplyr
+HousingData <- HousingData %>%
+  mutate(PriceCategory = ifelse(HousePrice > 21.20, "Expensive", "Cheap"))
+
+#See the price category ratio
+table(HousingData$PriceCategory)
+
 
 ### Exploration Data Analysis ###
 
@@ -43,15 +57,14 @@ par(mfrow = c(2, 2))
 hist(HousingData$AvgRoom)
 hist(HousingData$HouseAge)
 hist(HousingData$DistToCityCenter)
-hist(HousingData$IndustryProp)
-plot(HousingData$medv)
+hist(HousingData$HousePrice)
 
 #Cute vizualisasion using ggplot
 
 #Suppose we want vizualise Avg room with respect to House age
 RoomToAgePlot <- ggplot(HousingData, aes(x = DistToCityCenter, y = CrimeRate, color = as.factor(OldHouse))) + geom_point() + 
   scale_y_log10() + scale_x_log10() + 
-  ggtitle("Distance to City to Crime Rate plot") + xlab("Distance to City Center") + ylab("Crime rate") + facet_wrap(~chas)
+  ggtitle("Distance to City to Crime Rate plot") + xlab("Distance to City Center") + ylab("Crime rate")
 RoomToAgePlot
 
 RoomToAgePlot <- ggplot(HousingData, aes(x = DistToCityCenter, y = CrimeRate)) + geom_point(aes(color = AvgRoom)) + 
@@ -64,9 +77,9 @@ AgePlot <- ggplot(HousingData, aes(x = AvgRoom, fill = as.factor(OldHouse))) + g
   ggtitle("Average number of Rooms")
 AgePlot
 
-FacetPlot <- ggplot(HousingData, aes(x = AvgRoom, fill = as.factor(OldHouse))) + geom_histogram(binwidth = 0.5, position = "dodge") + 
-  ggtitle("Average number of Rooms") + facet_wrap(~as.factor(OldHouse))
-FacetPlot
+PriceAgePlot <- ggplot(HousingData, aes(x = HouseAge, y = HousePrice, color = as.factor(OldHouse))) + geom_point() +
+  scale_y_log10() + ggtitle("Average number of Rooms")
+PriceAgePlot
 
 #Create Correlation Matrix
 #compute correlation matrix
@@ -79,12 +92,17 @@ ggcorrplot(CorMat, hc.order = TRUE, type = "lower", lab = TRUE)
 
 ### Statistical Analysis ###
 
+#Split dataset into Training dataset and Testing dataset!
+SampleToSplit <- sample.split(HousingData$HousePrice, SplitRatio = 0.70)
+HousingTrainData <- subset(HousingData, SampleToSplit == T) #this partition is for TRAINING the model!
+HousingTestData <- subset(HousingData, SampleToSplit == F) #this partition is for TESTING the model!
+
 #Running linear model
-RegressionMod <- lm(medv ~ ., data = HousingData)
+RegressionMod <- lm(HousePrice ~ ., data = HousingTrainData)
 summary(RegressionMod)
 
-RegressionMod <- lm(medv ~ CrimeRate + IndustryProp + chas + nox + AvgRoom + DistToCityCenter + rad + 
-                      tax + ptratio + black + lstat, data = HousingData)
+RegressionMod <- lm(HousePrice ~ CrimeRate + chas + nox + AvgRoom + DistToCityCenter + rad + 
+                      tax + ptratio + black + lstat, data = HousingTrainData)
 summary(RegressionMod)
 
 #Model diagnostics
@@ -97,24 +115,31 @@ plot(RegressionMod)
 #Analyze multicollinearity
 car::vif(RegressionMod)
 
-RegressionMod <- lm(medv ~ CrimeRate + IndustryProp + chas + nox + AvgRoom + DistToCityCenter + rad:tax + 
-                      ptratio + black + lstat, data = HousingData)
+#Final regression model
+RegressionMod <- lm(HousePrice ~ CrimeRate + chas + nox + AvgRoom + DistToCityCenter + rad:tax + 
+                      ptratio + black + lstat, data = HousingTrainData)
 summary(RegressionMod)
 
-#Run model prediction
-ModelPrediction <- predict(object = RegressionMod, newdata = HousingData, type = "response")
+### Alright! at this point we've trained our regression model, what's next? TEST THE MODEL WITH UNSEEN DATA aka TESTING DATASET ###
+str(HousingTestData) #check the testing data first!
 
-#Attach predictions output from the model to the main data
-HousingData$PredictedMedv <- ModelPrediction
+#Run model prediction (this is the defining moment!!)
+HousingTestData$PredictedHousePrice <- predict(object = RegressionMod, newdata = HousingTestData[-14], type = "response") 
 
-#Compare the results between prediction and observed medv value
-MedvPredObsPlot <- ggplot(HousingData, aes(x = medv, y = PredictedMedv)) + geom_point() + stat_smooth(method = "lm") + ggtitle("Predicted Vs Observed")
-MedvPredObsPlot
+#Round to 2 decimal points for easy review
+HousingTestData$PredictedHousePrice <- round(HousingTestData$PredictedHousePrice, 2)
 
+#Compare the results between prediction and observed HousePrice value
+HousePricePredObsPlot <- ggplot(HousingTestData, aes(x = HousePrice, y = PredictedHousePrice)) + geom_point() + stat_smooth(method = "lm") + ggtitle("Predicted Vs Observed")
+HousePricePredObsPlot
 
+#FINALLY, let's compute the model's accuracy using Root Mean Squared Error (RMSE)
 
+Rmse_diag <- sqrt(mean((HousingTestData$PredictedHousePrice - HousingTestData$HousePrice)^2))
+Rmse_diag # Summary: On average, our model's prediction is different by USD 5.3k from the actual price!
 
+#congratulations you're now the next data scientist!!!
 
-
-
+#Feel free to contact me for questions or anything related to R
+# Email: kevintongam98@gmail.com
 
